ceb58f04a1fe8b0c48260aaa3fa73de05651806c
Matsuoka Kota <kota11250910@gmail.com>
2018-12-08 12:48:09 +0900

    [1208] LINE push message

---
+++ ac-1208-LINE-push-message/push_message_sender.py
@@ -0,0 +1,18 @@
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+
+LINE_CHANNEL_ACCESS_TOKEN = "ご自身のチャネルのアクセストークン"
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+
+
+def main():
+    user_id = "プッシュ通知を送りたLINEユーザーのuser_id"
+
+    messages = TextSendMessage(text=f"こんにちは😁\n\n"
+                                    f"最近はいかがお過ごしでしょうか?")
+    line_bot_api.push_message(user_id, messages=messages)
+
+
+if __name__ == "__main__":
+    main()
