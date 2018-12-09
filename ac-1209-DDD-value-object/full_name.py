c30c8f00992f8d595bf0f3cec34f896d17f2fd7b
Matsuoka Kota <kota11250910@gmail.com>
2018-12-09 17:32:54 +0900

    [1209] 同一の値オブジェクト同士を同じオブジェクトと判断できる

--- ac-1209-DDD-value-object/full_name.py
+++ ac-1209-DDD-value-object/full_name.py
@@ -6,5 +6,13 @@ class FullName:
     family_name: str
     first_name: str
 
-fullname1 = FullName("matsuoka", "kota")
-print(fullname1.family_name)
+    def __eq__(self, other):
+        return (self.first_name == other.first_name) and \
+               (self.family_name == other.family_name) and \
+               isinstance(other, FullName)
+
+
+full_name1 = FullName("matsuoka", "kota")
+full_name2 = FullName("tanaka", "hiroshi")
+print(full_name1 == full_name1)
+print(full_name1 == full_name2)
