c21c96f19071cd0e081fc02b8a4b33ce88fa2912
Matsuoka Kota <kota11250910@gmail.com>
2018-12-14 21:54:39 +0900

    [1214] テスト用のリポジトリの追加と依存の除去

---
+++ ac_1214_DDD_test_repository/programn.py
@@ -0,0 +1,19 @@
+import uuid
+
+from ac_1214_DDD_test_repository.in_memory_user_reposiotry import IUserRepository
+from ac_1214_DDD_test_repository.user import User, UserId, Username, FullName
+from ac_1214_DDD_test_repository.user_service import UserService
+
+
+class Program:
+    def creat_user(self, username: str, fist_name:str, family_name: str, user_repository: IUserRepository):
+        user_repository = user_repository
+        user = User(UserId(str(uuid.uuid4())),
+                    Username(username),
+                    FullName(fist_name, family_name))
+
+        user_service = UserService(user_repository)
+        if user_service.is_duplicated(user):
+            raise ValueError("重複しています")
+        else:
+            user_repository.save(user)
---
+++ ac_1214_DDD_test_repository/test/test_program.py
@@ -0,0 +1,25 @@
+import unittest
+
+from ac_1214_DDD_test_repository.in_memory_user_reposiotry import InMemoryUserRepository
+from ac_1214_DDD_test_repository.programn import Program
+from ac_1214_DDD_test_repository.user import Username
+
+
+class TestProgram(unittest.TestCase):
+    def test_usernameが重複時は保存できない(self):
+        in_memory_user_repository = InMemoryUserRepository()
+        with self.assertRaises(ValueError):
+            Program().creat_user("kota", "こうた", "まつい", in_memory_user_repository)
+
+    def test_usernameが重複していない時は保存してそのusernameが一件だけ存在するか(self):
+        username = "yuki"
+        in_memory_user_repository = InMemoryUserRepository()
+
+        Program().creat_user(username, "ゆうき", "まつい", in_memory_user_repository)
+        actual = in_memory_user_repository.find(Username(username))
+
+        self.assertEqual(username, actual.username.value)
+
+
+if __name__ == "__main__":
+    unittest.main()
